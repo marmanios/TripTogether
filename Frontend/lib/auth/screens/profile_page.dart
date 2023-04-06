6ae30dd2a3d14d8755169e0d5d729aa1288dd63c
Maged Armanios <67605395+marmanios@users.noreply.github.com>
2023-04-06 05:39:56 -0400

    ughhh

--- Frontend/lib/auth/screens/profile_page.dart
+++ Frontend/lib/auth/screens/profile_page.dart
@@ -4,6 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 User? currentUser = _auth.currentUser;
+String? phoneNumber;
+String? name;
+bool? isFemale;
+int? rating;
+
 Future<DocumentSnapshot> getUserData() async {
   return await FirebaseFirestore.instance
       .collection('users')
@@ -26,9 +31,13 @@ class ProfilePage extends StatelessWidget {
               if (snapshot.connectionState == ConnectionState.done) {
                 Map<String, dynamic> data =
                     snapshot.data!.data() as Map<String, dynamic>;
-                return Text('Welcome ${data['phoneNumber']}');
+                phoneNumber = data['phoneNumber'];
+                name = data['name'];
+                isFemale = data['isFemale'];
+                rating = data['rating'];
+                return Text('Welcome $phoneNumber');
               } else {
-                return CircularProgressIndicator();
+                return const CircularProgressIndicator();
               }
             },
           )
