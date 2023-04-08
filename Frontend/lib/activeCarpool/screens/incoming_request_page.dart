b7ef53fa9800d6a97e6c44e5034841f1ee4b1d94
Matthew <matthew_huynh09@yahoo.com>
2023-04-07 23:05:19 -0400

    added elements for incoming request popup

--- Frontend/lib/activeCarpool/screens/incoming_request_page.dart
+++ Frontend/lib/activeCarpool/screens/incoming_request_page.dart
@@ -1,95 +1,79 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutterapp/common/widgets/custom_button.dart';
+import '../../constants.dart';
 
-class MyPage extends StatefulWidget {
-  @override
-  _MyPageState createState() => _MyPageState();
-}
-
-class _MyPageState extends State<MyPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  String _textField1 = '';
-  String _textField2 = '';
-  String _textField3 = '';
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      // Do something with the form data
-      print('Text Field 1: $_textField1');
-      print('Text Field 2: $_textField2');
-      print('Text Field 3: $_textField3');
-    }
-  }
+class IncomingRequestPage extends StatelessWidget {
+  const IncomingRequestPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Page'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Text Field 1'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _textField1 = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Text Field 2'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _textField2 = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Text Field 3'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _textField3 = value!;
-                },
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    child: Text('Cancel'),
-                    onPressed: () {
-                      // Do something when the user cancels the form
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('Submit'),
-                    onPressed: _submitForm,
-                  ),
-                ],
-              ),
-            ],
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Incoming Request'),
+          backgroundColor: loginTitleColor,
           ),
-        ),
+        body: Column(
+          children: [
+          SizedBox(height: 10),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              'Passenger Name: placeholdername',
+              style: TextStyle(color:loginTitleColor),
+             ),
+          ),
+          // Spacer(flex: 1),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              'Passenger Rating: placeholderrating',
+              style: TextStyle(color:loginTitleColor),
+             ),
+            ),
+          // Spacer(flex: 1),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              'New Fare: placeholderfare',
+              style: TextStyle(color:loginTitleColor),
+            ),
+          ),
+          // Spacer(flex: 1),
+          CustomButton(text: "Accept", onTap: ()=>{}),
+          // Spacer(flex: 1),
+          CustomButton(text: "Reject", onTap: ()=>{}),
+        ]),
       ),
     );
   }
 }
+
+// class MyWidget extends StatelessWidget {
+//   final int numStars;
+
+//   MyWidget({required this.numStars});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Text('My Rating'),
+//         Row(
+//           children: buildStarIcons(numStars),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// List<Widget> buildStarIcons(int numStars) {
+//   List<Widget> stars = [];
+//   for (int i = 0; i < numStars; i++) {
+//     stars.add(Icon(Icons.star, color: Colors.yellow));
+//   }
+//   return stars;
+// }
