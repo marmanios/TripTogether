ba4886c6b67acc1a55962abe1b389a43985594e2
Matthew <matthew_huynh09@yahoo.com>
2023-04-09 21:06:59 -0400

    formatting elements on the screen

--- Frontend/lib/activeCarpool/screens/incoming_request_page.dart
+++ Frontend/lib/activeCarpool/screens/incoming_request_page.dart
@@ -3,10 +3,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/common/widgets/custom_button.dart';
+import 'package:flutterapp/common/widgets/m_button.dart';
 import '../../constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutterapp/common/widgets/custom_button.dart';
 import '../../constants.dart';
 import 'package:flutter/material.dart';
 
@@ -50,42 +50,57 @@ class IncomingRequestPage extends StatelessWidget {
                     decoration: const BoxDecoration(color: Colors.white)
                   ),
                   Column(children: [
-                    const SizedBox(height: 50),
+                    const SizedBox(height: 60),
                     Align(
                       alignment: Alignment.topCenter,
                       child: Text(
                         'Passenger Name: $name',
-                        style: TextStyle(color:loginTitleColor),
+                        style: TextStyle(
+                          color:loginTitleColor,
+                          fontSize:24
+                          ),
                       ),
                     ),
-                    Spacer(flex: 1),
+                    Spacer(flex: 2),
                     Align(
                       alignment: Alignment.topCenter,
                       child: Text(
                         'Passenger Phone Number: $phoneNumber',
-                        style: TextStyle(color:loginTitleColor),
+                        style: TextStyle(
+                          color:loginTitleColor,
+                          fontSize:18
+                          ),
                       ),
                       ),
-                    Spacer(flex: 1),
+                    Spacer(flex: 2),
                     Align(
                       alignment: Alignment.topCenter,
                       child: Text(
                         'Passenger Rating: $rating Stars',
-                        style: TextStyle(color:loginTitleColor),
+                        style: TextStyle(
+                          color:loginTitleColor,
+                          fontSize:18
+                          ),
                       ),
                       ),
-                    Spacer(flex: 1),
+                    Spacer(flex: 2),
                     Align(
                       alignment: Alignment.topCenter,
                       child: Text(
                         'New Fare: placeholderfare',
-                        style: TextStyle(color:loginTitleColor),
+                        style: TextStyle(
+                          color:loginTitleColor,
+                          fontSize:18
+                          ),
                       ),
                     ),
-                    Spacer(flex: 1),
-                    CustomButton(text: "Accept", onTap: ()=>{}),
-                    Spacer(flex: 1),
-                    CustomButton(text: "Reject", onTap: ()=>{}),
+                    Spacer(flex: 40),
+                    Row(children: [
+                      mButton(text: "Accept", onTap: ()=>{}, size: 200, buttoncolor: loginTitleColor, textColor: Colors.white,),
+                      Spacer(flex: 4),
+                      mButton(text: "Reject", onTap: ()=>{}, size: 200, buttoncolor: loginTitleColor, textColor: Colors.white,),
+                    ],),
+                    Spacer(flex:10),
                   ],)
                 ],);
               }else{
