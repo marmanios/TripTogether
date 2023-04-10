0c954279e096e643fd88ab939a0d2eb968b73203
Matthew <matthew_huynh09@yahoo.com>
2023-04-09 21:53:25 -0400

    changing button type

--- Frontend/lib/activeCarpool/screens/incoming_request_page.dart
+++ Frontend/lib/activeCarpool/screens/incoming_request_page.dart
@@ -3,7 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/common/widgets/custom_button.dart';
-import 'package:flutterapp/common/widgets/m_button.dart';
+import 'package:flutterapp/common/widgets/jeffe_button.dart';
 import '../../constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -96,9 +96,9 @@ class IncomingRequestPage extends StatelessWidget {
                     ),
                     Spacer(flex: 40),
                     Row(children: [
-                      mButton(text: "Accept", onTap: ()=>{}, size: 200, buttoncolor: loginTitleColor, textColor: Colors.white,),
+                      JeffeButton(text: "Accept", onTap: ()=>{}, size: 200, buttoncolor: loginTitleColor, textColor: Colors.white,),
                       Spacer(flex: 4),
-                      mButton(text: "Reject", onTap: ()=>{}, size: 200, buttoncolor: loginTitleColor, textColor: Colors.white,),
+                      JeffeButton(text: "Reject", onTap: ()=>{}, size: 200, buttoncolor: loginTitleColor, textColor: Colors.white,),
                     ],),
                     Spacer(flex:10),
                   ],)
